62b7dbe17b953348d8602ae7d82014ca1b917301
SATHVIK SVS <svssathvik77@gmail.com>
2024-02-14 17:35:41 +0530

    easy level questions and testcases finished

---
+++ constants/QB.js
@@ -0,0 +1,58 @@
+const easy = [
+    {
+        qId : "qs1",
+        question : `print("%d",5);`,
+        ansId : "ans1"
+    },
+    {
+        qId : "qs2",
+        question : `printf("%d",*5);`,
+        ansId : "ans2"
+    },
+    {
+        qId : "qs3",
+        question : `scanf("%d",#a);`,
+        ansId : "ans3"
+    },
+    {
+        qId : "qs4",
+        question : `for(int i=0;i<5;i+)`,
+        ansId : "ans4"
+    },
+    {
+        qId : "qs5",
+        question : `for(int i=0,i<5,i++)`,
+        ansId : "ans5"
+    },
+    {
+        qId : "qs6",
+        question : `for(int i=0;i<5,i++)`,
+        ansId : "ans5"
+    },
+    {
+        qId : "qs6",
+        question : `for(int i=0,i<5;i++)`,
+        ansId : "ans6"
+    },
+    {
+        qId : "qs7",
+        question : `for(int i=0,i<5;i++)`,
+        ansId : "ans7"
+    },
+    {
+        qId : "qs8",
+        question : `scanf("%[^n]",s);`,
+        ansId : "ans8"
+    },
+    {
+        qId : "qs9",
+        question : `scanf("%[\n]",s);`,
+        ansId : "ans9"
+    },
+    {
+        qId : "qs10",
+        question : `printf("%d",print("1 1"));`,
+        ansId : "ans10"
+    }
+];
+module.exports = {easy};
---
+++ constants/TC.js
@@ -0,0 +1,53 @@
+const ans = [
+    {
+        ansId : "ans1",
+        ans : `printf("%d",5);`,
+        score : 1
+    },
+    {
+        ansId : "ans2",
+        ans : `printf("%d",5);`,
+        score : 1
+    },
+    {
+        ansId : "ans3",
+        ans : `scanf("%d",&a);`,
+        score : 1
+    },
+    {
+        ansId : "ans4",
+        ans : `for(int i=0;i<5;i++)`,
+        score : 1
+    },
+    {
+        ansId : "ans5",
+        ans : `for(int i=0;i<5;i++)`,
+        score : 1
+    },
+    {
+        ansId : "ans6",
+        ans : `for(int i=0;i<5;i++)`,
+        score : 1
+    },
+    {
+        ansId : "ans7",
+        ans : `for(int i=0;i<5;i++)`,
+        score : 1
+    },
+    {
+        ansId : "ans8",
+        ans : `scanf("%[^\n]",s);`,
+        score : 1
+    },
+    {
+        ansId : "ans9",
+        ans : `scanf("%[^\n]",s);`,
+        score : 1
+    },
+    {
+        ansId : "ans10",
+        ans : `printf("%d",printf("1 1"));`,
+        score : 1
+    }
+];
+module.exports = {ans};
