8bcd5443c4ee37b869be5c2c6c5a79b2ebfbe919
sathishsara1 <108066377+Azeem-0@users.noreply.github.com>
2024-02-19 21:35:54 +0530

    set rollvalues to 0 when score is zero

--- controllers/updatesController.js
+++ controllers/updatesController.js
@@ -85,7 +85,7 @@ const setScoreZero = async (req, res) => {
     try {
         console.log(req.body);
         const { regNo } = req.body;
-        const user = await userModel.findOneAndUpdate({ regNo: regNo }, { $set: { score: 0, currPosition: 1, totalRolls: 0 } });
+        const user = await userModel.findOneAndUpdate({ regNo: regNo }, { $set: { score: 0, currPosition: 1, totalRolls: 0, rollValues:[] } });
         res.json({ message: "Reset Successfull", status: true, user: user });
         return;
     }
