cb81d8b9c944729c13885a14a15fbbc3aec5be55
Azeem Shaik <azeemshaik025@gmail.com>
2024-02-19 17:28:09 +0530

    Intialize controller to reset values

--- routes/updates.js
+++ routes/updates.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const Router = express.Router();
-const { updateScore, updatePosition,updateContestTimer,getContestTime } = require('../controllers/updatesController.js');
+const { updateScore, updatePosition, updateContestTimer, getContestTime, setScoreZero } = require('../controllers/updatesController.js');
 
 
 Router.post('/updatePosition', updatePosition);
-Router.post("/set-contest-time",updateContestTimer);
-Router.get("/get-contest-time",getContestTime);
+Router.post("/set-contest-time", updateContestTimer);
+Router.get("/get-contest-time", getContestTime);
+Router.post('/set-score-zero', setScoreZero);
 module.exports = Router;
