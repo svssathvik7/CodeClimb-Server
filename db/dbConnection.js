4a844459882d91f685c5cef387756d7e0955170e
SATHVIK SVS <101496703+svssathvik7@users.noreply.github.com>
2024-11-03 22:42:17 +0530

    Update dbConnection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // process.env will be used once we move to production
-mongoose.connect("mongodb+srv://sathishsara1007:PUTbv0P7vPmJQniW@cluster0.4nso06z.mongodb.net/codathon2024");
+mongoose.connect(process.env.DB);
 const db = mongoose.connection
 db.on("error", () => { console.log("Error connection to db") });
 db.once("open", () => { console.log("Successfully connected to database") });
