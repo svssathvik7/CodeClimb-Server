bfacde6050c86175985c908ced912f90dbfb783e
SATHVIK SVS <svssathvik77@gmail.com>
2025-03-23 10:42:37 +0530

    fix login and register routes

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,81 +1,90 @@
-require('dotenv').config();
-const userModel = require('../models/userModel');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 // we expect isAdmin as "true" or "false"
 const registerUser = async (req, res) => {
-    try {
-        const { username, password, isAdmin } = req.body.formData;
-        const regNo = username;
-        const checkUser = await userModel.findOne({ regNo: regNo });
-        if (!checkUser) {
-            password = await bcrypt(password,10);
-            const newUser = new userModel({
-                regNo: regNo,
-                password: password,
-                isAdmin : isAdmin,
-                score: 0,
-                totalRolls: 0,
-                currPosition: 1,
-                bonus: 0,
-                questions: {
-                    easy: [],
-                    medium: [],
-                    hard: []
-                },
-            });
-            await newUser.save();
-            return res.json({ message: "Successfully registered", status: true });
-        }
-        else {
-            return res.json({ message: "User already registered!", status: false });
-        }
-    } catch (error) {
-        console.log(error.message);
-        return res.json({ message: "Error Occured", status: false });
+  try {
+    const { username, password, isAdmin } = req.body.formData;
+    const regNo = username;
+    const checkUser = await userModel.findOne({ regNo: regNo });
+    if (!checkUser) {
+      const newUser = new userModel({
+        regNo: regNo,
+        password: password,
+        isAdmin: isAdmin,
+        score: 0,
+        totalRolls: 0,
+        currPosition: 1,
+        bonus: 0,
+        questions: {
+          easy: [],
+          medium: [],
+          hard: [],
+        },
+      });
+      await newUser.save();
+      return res.json({ message: "Successfully registered", status: true });
+    } else {
+      return res.json({ message: "User already registered!", status: false });
     }
-}
+  } catch (error) {
+    console.log(error.message);
+    return res.json({ message: "Error Occured", status: false });
+  }
+};
 const loginUser = async (regNo, password) => {
-    try {
-        const user = await userModel.findOne({ regNo: regNo });
-        if (user === null) {
-            return false;
-        }
-        const match = await bcrypt.compare(password,user.password);
-        if (match) {
-            const token = jwt.sign({
-                id: user._id,
-                regNo: user.regNo,
-                isAdmin : user.isAdmin
-            }, process.env.SECRET, { expiresIn: '60m' });
-            return [true, token];
-        }
-        else {
-            return false;
-        }
+  try {
+    const user = await userModel.findOne({ regNo: regNo });
+    if (user === null) {
+      return false;
     }
-    catch (err) {
-        console.log('Error', err.message);
-        return false;
+    const match = password === user.password;
+    if (match) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+          regNo: user.regNo,
+          isAdmin: user.isAdmin,
+        },
+        process.env.SECRET,
+        { expiresIn: "60m" }
+      );
+      return [true, token];
+    } else {
+      return false;
     }
-}
+  } catch (err) {
+    console.log("Error", err.message);
+    return false;
+  }
+};
 
 const createUsers = async (req, res) => {
-    // we expect users object where each user has userID and isAdmin value
-    try {
-        const { users } = req.body;
-        const commonHashPass = bcrypt("codathon",10);
-        for (const user of users) {
-
-            await userModel.create({ regNo: user.userID, password: commonHashPass,isAdmin : (user.isAdmin == "true" ? true : false) });
-            console.log(`User ${user.userID} created successfully`);
-        }
-        return res.json({ message: 'All users created successfully', status: true })
-    } catch (error) {
-        console.error('Error creating users:', error);
-        return res.json({ message: "Error occured while creating accounts", status: false });
+  // we expect users object where each user has userID and isAdmin value
+  try {
+    const { users } = req.body;
+    const commonHashPass = bcrypt("codathon", 10);
+    for (const user of users) {
+      await userModel.create({
+        regNo: user.userID,
+        password: commonHashPass,
+        isAdmin: user.isAdmin == "true" ? true : false,
+      });
+      console.log(`User ${user.userID} created successfully`);
     }
+    return res.json({
+      message: "All users created successfully",
+      status: true,
+    });
+  } catch (error) {
+    console.error("Error creating users:", error);
+    return res.json({
+      message: "Error occured while creating accounts",
+      status: false,
+    });
+  }
 };
 
 module.exports = { loginUser, registerUser, createUsers };
--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 // process.env will be used once we move to production
 mongoose.connect(process.env.DB);
-const db = mongoose.connection
-db.on("error", () => { console.log("Error connection to db") });
-db.once("open", () => { console.log("Successfully connected to database") });
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("Error connection to db");
+});
+db.once("open", () => {
+  console.log("Successfully connected to database");
+});
 
 module.exports = db;
